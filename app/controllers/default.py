cae8d1ba17404838b534bf6ef2f512b8eb246423
JonathanLima25 <jonathan.ferreira6@fatec.sp.gov.br>
2020-10-11 18:04:43 -0300

    Adicionando rotas de teste

--- app/controllers/default.py
+++ app/controllers/default.py
@@ -1,6 +1,15 @@
 from app import app
 
 
+@app.route('/index')
 @app.route('/')
 def index():
     return 'Hello world'
+
+@app.route('/test')
+@app.route('/test/<name>')
+def teste(name=None):
+    if name:
+        return '<h1>Olá, %s!</h1>' %name
+    else:
+        return '<h1>Olá!</h1>'
