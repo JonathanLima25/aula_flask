4cea8b6e555dcf19828c009a8204fe7ebe861637
JonathanLima25 <jonathan.ferreira6@fatec.sp.gov.br>
2020-10-11 16:02:31 -0300

    Adicionando as configurações e o debugger

--- app/__init__.py
+++ app/__init__.py
@@ -4,12 +4,14 @@ from flask_migrate import Migrate, MigrateCommand
 from flask_script import Manager
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///storage.db'
+
 db = SQLAlchemy(app)
+
 migrate = Migrate(app, db)
 
 manager = Manager(app)
 manager.add_command('db', MigrateCommand)
 
+app.config.from_object('config')
 
 from app.controllers import default
